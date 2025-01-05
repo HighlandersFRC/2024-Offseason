2bb6aad2e3a3b1970c2546ce115a66edf407de95
RishabhK12 <rishabhkapoor@live.com>
2025-01-05 10:05:58 -0700

    Added commands to auto map

--- 2025-Vision/src/main/java/frc/robot/RobotContainer.java
+++ 2025-Vision/src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,10 @@ import org.json.JSONTokener;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.DoNothing;
@@ -55,6 +58,8 @@ public class RobotContainer {
       put("Wait", () -> new DoNothing());
       put("Print", () -> new PrintCommand("10s"));
       put("Full Send", () -> new FullSendFollower(drive, null, false));
+      put("Place Coral High", () -> new ParallelCommandGroup(new SetRobotState(superstructure, SuperState.ELEVATOR_UP), new SequentialCommandGroup(new WaitCommand(1.0), new MoveTestMotor(motorTest, "motor1", 0.3))));
+      put("Intake Coral", () -> new MoveTestMotor(motorTest, "motor1", -0.3));
     }
   };
 
